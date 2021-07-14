1f2da191ab74caf1a54aea274e576ace12da604f
ishevelkov <ishevelkov@gmail.com>
2021-07-14 15:11:17 +1000

    Initial Commit

---
+++ ShevelkovMA_HW3.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShevelkovMA_HW3.3
+//
+//  Created by Михаил Шевельков on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
